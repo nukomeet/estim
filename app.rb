c487760dba1e3d4cf5320667c646dd7c19b70231
Albanlv <albanlv@gmail.com>
2013-10-23 19:22:47 +0200

    first commit

--- app.rb
+++ app.rb
@@ -9,6 +9,18 @@ class SinatraBootstrap < Sinatra::Base
     haml :index
   end
 
+post '/estim' do
+  @client_name = params[:client_name]
+  puts @client_name
+  @mission = params[:mission]
+  puts @mission
+  @rate = params[:rate]
+  puts @rate
+  @hosting = params[:hosting]
+  puts @hosting
+  haml :estim
+end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
